9bd5d25ba4a5696db2e785410b34f8b9df56ff62
PH4MTOM <simonmadsen99@gmail.com>
2024-09-10 16:05:28 +0200

    WIP. Changes

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@
 
 import express from 'express';
 import cors from "cors";
-import records from "./records.js";
+//import records from "./records.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records)
+//app.use("/record", records)
 
 // start the Express server
 app.listen(PORT, () => {
