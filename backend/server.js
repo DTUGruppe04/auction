c51d9706772f6b6c535e883aa94b7d0bafd03204
PH4MTOM <simonmadsen99@gmail.com>
2024-09-10 15:56:30 +0200

    WIP. Setting up project

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// WIP. Still need to setup database, database connection and the backend server part. Ignore this code for now.
+
+import express from 'express';
+import cors from "cors";
+import records from "./records.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records)
+
+// start the Express server
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
