4d2b3784737ac25285fb9e5898c61462e9ddfd48
valde12 <s224289@DTU.dk>
2024-09-19 15:02:45 +0200

    mongodb connection to atlas cluster setup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
-// WIP. Still need to setup database, database connection and the backend server part. Ignore this code for now.
-
-import express from 'express';
+import express from "express";
 import cors from "cors";
-//import records from "./records.js";
+import records from "./routes/records.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use("/record", records)
+app.use("/records", records);
 
 // start the Express server
 app.listen(PORT, () => {
