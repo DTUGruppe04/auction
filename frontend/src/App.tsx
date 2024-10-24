b6b664764eaad3cfa74750b63fcb4aed7f5f7900
PH4MTOM <simonmadsen99@gmail.com>
2024-10-24 15:54:52 +0200

    Setup Flowbite-React with Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Slideshow from "./components/Carousel.tsx";
 
 const App = () => {
     return (
         <div className="w-full p-6">
             <Navbar />
             <Outlet />
+            <Slideshow />
         </div>
     );
 };
