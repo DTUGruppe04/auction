9000759daf8de78698a0b4e990c74f93262987e1
PH4MTOM <simonmadsen99@gmail.com>
2024-10-31 14:25:22 +0100

    WIP

--- frontend/src/components/Slideshow.tsx
+++ frontend/src/components/Slideshow.tsx
@@ -3,8 +3,8 @@ import { Carousel } from "flowbite-react"
 
 function Slideshow() {
     return (
-        <div className="flex justify-between p-4 bg-gray-100">
-            <div className="flex-1 h-96 sm:h-64 xl:h-80 2xl:h-96">
+        <div className="flex justify-between p-4 bg-gray-100 space-x-4">
+            <div className="flex-grow h-96 sm:h-64 xl:h-80 2xl:h-96">
                 <Carousel slideInterval={5000}>
                     <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..."/>
                     <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..."/>
@@ -13,8 +13,11 @@ function Slideshow() {
                     <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..."/>
                 </Carousel>
             </div>
-            <div className="flex-1 text-left">
-                <p>Text</p>
+            <div className="flex-grow-0 text-left w-2/5">
+                <div className="p-4 bg-stone-400">
+                    <p className="text-xl">Auction</p>
+                    <p className="text-sm">Est: $400 - $1200 USD</p>
+                </div>
             </div>
         </div>
     );
